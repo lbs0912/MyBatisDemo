87af3431f971d465b46487306079f0f9b2f520e0
lbs0912 <lbs1203940926@163.com>
2020-04-16 00:23:31 +0800

    mybatis demo

---
+++ src/main/java/com/lbs0912/app/Main.java
@@ -0,0 +1,55 @@
+package com.lbs0912.app;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.lbs0912.model.Country;
+
+
+public class Main {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    /**
+     * 初始化sqlSessionFactory
+     */
+    public static void init() {
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            reader.close();
+        } catch (IOException ignore) {
+            ignore.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // 连接数据库
+        init();
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            List<Country> countryList = sqlSession.selectList("selectAll");
+            printCountryList(countryList);
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    /**
+     * 打印country信息
+     * @param countryList
+     */
+    private static void printCountryList(List<Country> countryList) {
+        for (Country country : countryList) {
+            System.out.printf("%-4d%4s%4s\n", country.getId(), country.getCountryname(), country.getCountrycode());
+        }
+    }
+}
